c467f535fff5d6873f7ee439eabd504148fe18b7
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2024-03-24 22:49:21 -0300

    fix start

--- Assets/MenuPrincipal.cs
+++ Assets/MenuPrincipal.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicial : MonoBehaviour
 {
-    public void Start()
+    public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
